d6e71380184aecb568eb109f4545232aa6cd0bcf
AnonimAnonim2245 <98339220+AnonimAnonim2245@users.noreply.github.com>
2023-03-15 19:41:09 +0200

    It is only for mobile devices.

---
+++ Example3/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+using Example3.ViewModel;
+
+namespace Example3;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+    
+
+}
+
---
+++ Example3/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Example3;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
